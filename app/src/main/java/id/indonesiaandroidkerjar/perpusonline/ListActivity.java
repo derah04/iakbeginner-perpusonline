9e5827ac6ad26eaa70fd7eabf7023e43c9da6c54
gedew <igrwijaya@yadnyastudio.com>
2017-11-26 09:09:53 +0800

    update

--- app/src/main/java/id/indonesiaandroidkerjar/perpusonline/ListActivity.java
+++ app/src/main/java/id/indonesiaandroidkerjar/perpusonline/ListActivity.java
@@ -13,38 +13,9 @@ import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
 
-    ListView listBuku;
-    List<String> arrayBook = new ArrayList<String>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-
-        listBuku = (ListView) findViewById(R.id.listBuku);
-
-        final String[] staticBookTitle = new String[]{
-                "Bali",
-                "Jogja",
-                "Bandung",
-                "Lombok",
-                "Jakarta"
-        };
-
-        for (String book : staticBookTitle) {
-            arrayBook.add(book);
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayBook);
-        listBuku.setAdapter(adapter);
-
-        listBuku.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                String name = arrayBook.get(i);
-                Toast.makeText(ListActivity.this, name, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
