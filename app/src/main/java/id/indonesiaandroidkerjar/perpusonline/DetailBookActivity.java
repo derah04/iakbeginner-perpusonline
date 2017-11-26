7c2213182a10c4ab39ec05865670da025708fdf2
gedew <igrwijaya@yadnyastudio.com>
2017-11-26 14:04:38 +0800

    button pinjam clicked at detail book activity

--- app/src/main/java/id/indonesiaandroidkerjar/perpusonline/DetailBookActivity.java
+++ app/src/main/java/id/indonesiaandroidkerjar/perpusonline/DetailBookActivity.java
@@ -1,7 +1,9 @@
 package id.indonesiaandroidkerjar.perpusonline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,6 +14,8 @@ public class DetailBookActivity extends AppCompatActivity {
     TextView txtHarga;
     TextView txtDescription;
     Button btnPinjam;
+    String namaPenulis = "Anonymous";
+    String hrgSewa = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +27,38 @@ public class DetailBookActivity extends AppCompatActivity {
         txtDescription = (TextView) findViewById(R.id.txtDescription);
         txtHarga = (TextView) findViewById(R.id.txtHargaSewa);
         btnPinjam = (Button) findViewById(R.id.btnPinjam);
+
+        String bookTitle = getIntent().getStringExtra("book_title");
+
+        if (bookTitle.equals("Buku 1")){
+            namaPenulis = "Eko";
+            hrgSewa = "10.000";
+        }else if(bookTitle.equals("Buku 2")){
+            namaPenulis = "Budi";
+            hrgSewa = "20.000";
+        }
+
+        txtNamaBuku.setText(bookTitle);
+        txtPenulis.setText(namaPenulis);
+        txtHarga.setText(hrgSewa);
+
+        String descriptions = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        for (int i = 0; i < 5; i++){
+            descriptions += descriptions; // description = description + description;
+        }
+
+        txtDescription.setText(descriptions);
+
+        btnPinjam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bookIntent = new Intent(
+                        DetailBookActivity.this,
+                        PinjamActivity.class);
+
+                bookIntent.putExtra("hrg_sewa", hrgSewa);
+                startActivity(bookIntent);
+            }
+        });
     }
 }
