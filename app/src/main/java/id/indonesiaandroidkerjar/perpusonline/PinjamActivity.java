c0b61c6eeb384c618898a95e2116c2c8297b6bdb
gedew <igrwijaya@yadnyastudio.com>
2017-11-26 14:22:01 +0800

    pinjam activity

--- app/src/main/java/id/indonesiaandroidkerjar/perpusonline/PinjamActivity.java
+++ app/src/main/java/id/indonesiaandroidkerjar/perpusonline/PinjamActivity.java
@@ -1,15 +1,20 @@
 package id.indonesiaandroidkerjar.perpusonline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PinjamActivity extends AppCompatActivity {
 
     EditText txtName;
     EditText txtPhone;
     Button btnPinjam;
+    TextView hrgSewa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,33 @@ public class PinjamActivity extends AppCompatActivity {
         txtName = (EditText) findViewById(R.id.txtNama);
         txtPhone = (EditText) findViewById(R.id.txtPhone);
         btnPinjam = (Button) findViewById(R.id.btnSimpanPinjam);
+        hrgSewa = (TextView) findViewById(R.id.txtPrice);
+
+        String getPrice = getIntent().getStringExtra("hrg_sewa");
+        hrgSewa.setText(getPrice);
+
+        btnPinjam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = txtName.getText().toString();
+                String phone = txtPhone.getText().toString();
+                validateOrder(name, phone);
+            }
+        });
+    }
+
+    //method
+    private void validateOrder(String name, String phone)
+    {
+        if (name.equals("")){
+            //Jika name kosong
+            Toast.makeText(this, "Name is required!", Toast.LENGTH_SHORT).show();
+        }else if(phone.equals("")){
+            //jika phone kosong
+            Toast.makeText(this, "Phone is required!", Toast.LENGTH_SHORT).show();
+        }else{
+            //jika name dan phone TIDAK kosong
+            Toast.makeText(this, "Thanks", Toast.LENGTH_LONG).show();
+        }
     }
 }
